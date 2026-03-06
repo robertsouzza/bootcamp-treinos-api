2b7ca1a7abe3d8fb81b0ccee7e0ce3a4232aca96
robertsouzza <rcsouzza@gmail.com>
2026-03-06 15:33:17 -0400

    feat: add fastity api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World ");
+import Fastify from "fastify";
+import "dotenv/config";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "word" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
